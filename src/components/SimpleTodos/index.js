7ba618933579e44f359074d0f91d945c014c8f26
PrashanthKumaRerddy <reddyprashanth512@gmail.com>
2023-11-05 06:50:35 +0000

    solution

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,52 @@
+import {Component} from 'react'
+import './index.css'
+//import './components/SimpleTodos'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+// Write your code here
+class SimpleTodos extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="main-heading">Simple Todos</h1>
+        </div>
+      </div>
+    )
+  }
+}
+export default SimpleTodos
